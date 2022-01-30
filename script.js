389e8721fe2fe1a3497a1ea22210701e98c38970
gauravyadav6294 <94858762+gauravyadav6294@users.noreply.github.com>
2022-01-30 19:34:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+var newURL = "http://universities.hipolabs.com/search?";
+var table = document.getElementById("table");
+
+fetch(newURL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    for (let i = 0; i < 100; i++) {
+         tr = document.createElement("tr");
+         td1 = document.createElement("td");
+         td2 = document.createElement("td");
+        
+        tr.innerText = data[i].name;
+        td1.innerText = data[i].domains;
+        td2.innerText = data[i].country;
+
+        tr.append(td1, td2);         
+        table.appendChild(tr);
+        
+
+    }
+  })
+
+  .catch((err) => console.log("API is failed. Please check the issue and try again."));
+
+const searchFun = () => {
+let filter = document.getElementById('myInput').value.toUpperCase();
+let myTable = document.getElementById('table');
+let tr = myTable.getElementsByTagName('tr');
+
+for(var i=0; i<tr.length; i++){
+  let td = tr[i].getElementsByTagName('td')[0];
+
+  if(td){
+    let textvalue = td.textContent || td.innerHTML;
+    if(textvalue.toUpperCase().indexOf(filter)>-1){
+        tr[i].style.display = "";
+    }else{
+      tr[i].style.display = "none";
+    }
+  }
+}
+}
